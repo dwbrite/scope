4cf5ed41328e14a91fb7cc4e51db40c4bb24fb87
devin <dwbrite@gmail.com>
2017-10-19 15:12:46 -0400

    Add base for RigidListView

---
+++ src/gui/windows/primary/controls/RigidListView.java
@@ -0,0 +1,14 @@
+package gui.windows.primary.controls;
+
+import javafx.scene.layout.VBox;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class RigidListView<T> extends ScrollPane {
+
+    VBox items = new VBox();
+
+    public RigidListView() {
+    }
+}
